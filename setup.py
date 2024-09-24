211728f26e306e5d9f28efb53e726a8daa8af8e5
Kanwal Arora <kanwalarora029@gmail.com>
2024-09-24 14:14:38 +0100

    Updated name and install_requires

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='tradingbot',
+    name='llmtradingbot',
     version='0.0.1',
     author="Kanwal Arora",
     author_email="kanwalarora029@gmail.com",
     packages=find_packages(),
+    install_requires=["langchain","langchain-openai","langchain-astradb","datasets","pypdf","python-dotenv","flask"]
 )
